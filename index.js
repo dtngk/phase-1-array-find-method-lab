72fcb12cfc498df626afa70a293ac88726461a6b
Dat Nguyen <dnguyenk2@gmail.com>
2022-03-26 15:36:36 -0700

    Compeleted assignment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code your solution here
 function superbowlWin(arr){
 
-    const obj = arr.find(ele =>  ele.result === 'W');
+    const obj = arr.find(ele =>  ele.result === "W");
 
     if (obj == null){
         return obj;
