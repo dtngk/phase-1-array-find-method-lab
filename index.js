321350015d277f81a0c3d59681d5836a489213b4
Dat Nguyen <dnguyenk2@gmail.com>
2022-03-26 15:34:46 -0700

    Compeleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(arr){
+
+    const obj = arr.find(ele =>  ele.result === 'W');
+
+    if (obj == null){
+        return obj;
+    }
+
+    return obj.year;
+}
